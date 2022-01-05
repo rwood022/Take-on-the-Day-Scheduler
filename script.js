7ab177fcd6d1db7689e0a57b09f828d60a81babb
Rebecca Wood <rwood022@gmail.com>
2022-01-05 08:08:49 -0500

    change save buttons

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-var today = moment();
+// var textBoxEl = $("past", "present", "future")
 var hourInputEl = $("hourInput"); 
+var saveBtn = $(".saveBtn");
 
 
+//Current Day, Date, and Time
+var today = (moment().format("dddd, MMMM Do YYYY"));
+$("#currentDay").text(today);
 
-$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
+//Text in textbox
 var printHourInput = function (input) {
     var listEl = $("<li>");
     listEl.addClass("list-group-item").text(listEl);
     listEl.appendTo(hourInputEl);
 };
 
+// Save input in textbox
 var handleInputSave = function (event) {
     event.preventDefault ();
 
@@ -18,7 +23,43 @@ var handleInputSave = function (event) {
 
     printHourInput(hourInput);
 
-    //resets hour input
-    hourInputEl.val("");
+   
 }
+
+saveBtn.on("save", handleInputSave);
+
+
+// Color change by time
+function blockColor(){
+    var time = moment().hours();
+// for loop $.each("CLASS NAME"), then add logic
+
+    $(".task").each(function() {
+        var presentHour = parseInt($(this).attr("id"));
+        console.log(presentHour);
+
+        if (presentHour > time) {
+            $(this).addClass("past");
+        } else if (presentHour === time) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present")
+            $(this).addClass("future");
+        }
+    })
+};
+
+// Save task input into local storage
+saveBtn.on("click", function() {
+    var hour = $(this).siblings(".hour").text();
+    var task = $(this).siblings(".task").val();
+
+localStorage.setItem(hour, task);
+})
+
+localStorage.getItem(hour, task);
+blockColor();
+
 
