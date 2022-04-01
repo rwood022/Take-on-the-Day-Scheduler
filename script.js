30aa09d82082815a4f49ceb142f7f1d0ab129b7a
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-04-01 18:51:54 -0400

    load saved data from local storage

--- script.js
+++ script.js
@@ -59,6 +59,17 @@ saveBtn.on("click", function() {
 localStorage.setItem(hour, task);
 })
 
+  // load any saved data from localStorage
+  $('#9 .description').val(localStorage.getItem('9'));
+  $('#10 .description').val(localStorage.getItem('10'));
+  $('#11 .description').val(localStorage.getItem('11'));
+  $('#12 .description').val(localStorage.getItem('12'));
+  $('#1 .description').val(localStorage.getItem('1'));
+  $('#2 .description').val(localStorage.getItem('2'));
+  $('#3 .description').val(localStorage.getItem('3'));
+  $('#4 .description').val(localStorage.getItem('4'));
+  $('#5 .description').val(localStorage.getItem('5'));
+
 localStorage.getItem(hour, task);
 blockColor();
 
